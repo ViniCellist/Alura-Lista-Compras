652fb58c1ce0d95c9f6185d3dd3e1fa50862fd49
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-13 00:16:57 -0300

    feat: main list item

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ let itemsList = [];
 
 const form = document.getElementById('form-itens');
 const itensInput = document.getElementById('receber-item');
+const ulItems = document.getElementById('lista-de-itens');
+const ulItemsBuyed = document.getElementById('itens-comprados');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     saveItem();
+    showItem();
+    itensInput.focus();
 });
 
 function saveItem() {
@@ -16,9 +20,52 @@ function saveItem() {
         alert('Item já existe');
     } else {
         itemsList.push({
-            valor: buyItem
+            valor: buyItem,
+            checar: false
         });
     };
 
-    console.log(itemsList);
+    itensInput.value = '';
+};
+
+function showItem() {
+    ulItems.innerHTML = '';
+    ulItemsBuyed.innerHTML = '';
+    itemsList.forEach((e, index) => {
+        if(e.checar) {
+            ulItemsBuyed.innerHTML += `
+                <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" checked class="is-clickable" />  
+                        <span class="itens-comprados is-size-5">${e.valor}</span>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
+                </li>
+            `;
+        } else {
+            ulItems.innerHTML += `
+                    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+                    <div>
+                        <input type="checkbox" class="is-clickable" />
+                        <input type="text" class="is-size-5" value="${e.valor}"></input>
+                    </div>
+                    <div>
+                        <i class="fa-solid fa-trash is-clickable deletar"></i>
+                    </div>
+                </li>
+            `;
+        };
+    });
+
+    const inputCheck = document.querySelectorAll('input[type="checkbox"]');
+    inputCheck.forEach(i => {
+        i.addEventListener('click', (e) => {
+            const elementValue = e.target.parentElement.parentElement.getAttribute('data-value');
+            itemsList[elementValue].checar = e.target.checked
+            showItem();
+        });
+    });
+
 };
